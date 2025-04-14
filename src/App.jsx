6361a238b4eaa406d88a6c06f5ef0cc71d144c24
simzeee <johnsims.dev@gmail.com>
2025-04-14 19:10:18 -0400

    feature: past orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
 const App = () => {
   const router = createRouter({ routeTree });
+  const queryClient = new QueryClient();
 
   return (
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
