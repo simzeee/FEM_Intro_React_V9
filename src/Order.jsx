7afd4a2b4ecbf30956b2136b169a65bbbb28f85b
simzeee <johnsims.dev@gmail.com>
2025-04-14 16:17:56 -0400

    feature: add checkout

--- src/Order.jsx
+++ src/Order.jsx
@@ -15,6 +15,21 @@ export default function Order() {
   const [loading, setLoading] = useState(true);
   const [cart, setCart] = useState([]);
 
+  async function checkout() {
+    setLoading(true);
+
+    await fetch("/api/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ cart }),
+    });
+
+    setCart([]);
+    setLoading(false);
+  }
+
   let price, selectedPizza;
   if (!loading) {
     selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
@@ -116,7 +131,7 @@ export default function Order() {
           )}
         </form>
       </div>
-      {loading ? <h2>LOADING...</h2> : <Cart cart={cart} />}
+      {loading ? <h2>LOADING...</h2> : <Cart checkout={checkout} cart={cart} />}
     </div>
   );
 }
